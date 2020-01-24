e4a7885ac7627b6569a39b935dfdc6bb65e3e9e2
rubalraj <rubalraj9604@gmail.com>
2020-01-25 02:04:31 +0530

    Calling..

---
+++ gen/com/example/phonecall/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.phonecall;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/phonecall/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.phonecall;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	EditText et;
+	Button b;
+	
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et=(EditText)findViewById(R.id.editText1);
+        b=(Button)findViewById(R.id.button1);
+        b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				String no=et.getText().toString();
+				Intent i=new Intent(Intent.ACTION_CALL,Uri.parse("tel:"+no));
+				startActivity(i);
+				
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
